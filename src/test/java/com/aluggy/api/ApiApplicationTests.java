1e6ace6b308814609b87da4a8d2e59b7368635a6
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-05-17 00:57:27 -0300

    feat: add CI workflow with GitHub Actions

--- src/test/java/com/aluggy/api/ApiApplicationTests.java
+++ src/test/java/com/aluggy/api/ApiApplicationTests.java
@@ -3,11 +3,18 @@ package com.aluggy.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void given2Plus2ShouldReturn4() {
+		assertEquals(4, 2+3);
 	}
 
 }
