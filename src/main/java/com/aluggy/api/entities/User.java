a6a719a0fc0b6f1a639d17e612aaafd782d8ce4c
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-05-22 00:57:37 -0300

    feat: add User and ProfilePhoto entities with Role enum

---
+++ src/main/java/com/aluggy/api/entities/User.java
@@ -0,0 +1,48 @@
+package com.aluggy.api.entities;
+
+import com.aluggy.api.entities.enums.Role;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.dialect.type.PostgreSQLEnumJdbcType;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    private Role role;
+    private String userName;
+    private String fullName;
+    private String emailAddress;
+    private String password;
+    private String contactNumber;
+    private String description;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant registeredAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant deletedAt;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profile_photo_id", referencedColumnName = "id")
+    ProfilePhoto profilePhoto;
+}
