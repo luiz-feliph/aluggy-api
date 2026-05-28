e852a53afb5c50bb41e250183f1ddeb017ce0191
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-05-28 02:01:31 -0300

    feat: implement role constructor and getRole()

--- src/main/java/com/aluggy/api/entities/enums/Role.java
+++ src/main/java/com/aluggy/api/entities/enums/Role.java
@@ -1,6 +1,16 @@
 package com.aluggy.api.entities.enums;
 
 public enum Role {
-    USER,
-    ADMIN
+    USER("user"),
+    ADMIN("admin");
+
+    private String role;
+
+    Role(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
