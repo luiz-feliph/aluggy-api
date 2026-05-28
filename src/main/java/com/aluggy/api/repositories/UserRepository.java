a91793ceec2e3ed85d172341316bbfa11b234d75
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-05-28 02:07:27 -0300

    feat: add findByUserName()

--- src/main/java/com/aluggy/api/repositories/UserRepository.java
+++ src/main/java/com/aluggy/api/repositories/UserRepository.java
@@ -2,8 +2,11 @@ package com.aluggy.api.repositories;
 
 import com.aluggy.api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    UserDetails findByUserName(String userName);
 }
