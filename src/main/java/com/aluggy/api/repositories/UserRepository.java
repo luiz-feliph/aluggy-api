07e033563a928b51e19e0d38800a6c8ff1b1ae3a
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-05-30 00:57:55 -0300

    feat: implement login with username or email

--- src/main/java/com/aluggy/api/repositories/UserRepository.java
+++ src/main/java/com/aluggy/api/repositories/UserRepository.java
@@ -4,9 +4,10 @@ import com.aluggy.api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    UserDetails findByUserName(String userName);
+    Optional<User> findByUsernameOrEmail(String userName, String emailAddress);
 }
