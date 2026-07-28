cf0882e0a8a0810b08a52262245da213f9f98db8
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-07-28 16:14:45 -0300

    feat: implement generate and validate JWT Token methods

--- src/main/java/com/aluggy/api/services/TokenService.java
+++ src/main/java/com/aluggy/api/services/TokenService.java
@@ -1,4 +1,50 @@
 package com.aluggy.api.services;
 
+import com.aluggy.api.entities.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+
+            return token;
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Error while generating token", e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return "";
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 }
