405fb236f890d47317cfca8b6920829211df26d9
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-08-11 01:48:17 -0300

    fix: remove role field from RegisterRequestDTO

--- src/main/java/com/aluggy/api/dto/RegisterRequestDTO.java
+++ src/main/java/com/aluggy/api/dto/RegisterRequestDTO.java
@@ -1,6 +1,4 @@
 package com.aluggy.api.dto;
 
-import com.aluggy.api.entities.enums.Role;
-
-public record RegisterRequestDTO(String userName, String fullName, String emailAddress, String contactNumber, String password, Role role) {
+public record RegisterRequestDTO(String userName, String fullName, String emailAddress, String contactNumber, String password) {
 }
