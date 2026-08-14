3807545ef066778f2ea7e9eef08e749d7b77bf2a
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-08-14 00:56:13 -0300

    fix: use HTTPS for production origin in CORS configuration

--- src/main/java/com/aluggy/api/infra/security/SecurityConfigurations.java
+++ src/main/java/com/aluggy/api/infra/security/SecurityConfigurations.java
@@ -66,7 +66,7 @@ public class SecurityConfigurations {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://aluggyapp.com.br"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://aluggyapp.com.br"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
