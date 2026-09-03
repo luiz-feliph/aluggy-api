6ab14c2720ff2289fc20e10d0f441219f1890d3d
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-09-03 02:37:56 -0300

    feat: implement /me endpoint to return authenticated user data

--- src/main/java/com/aluggy/api/controllers/AuthenticationController.java
+++ src/main/java/com/aluggy/api/controllers/AuthenticationController.java
@@ -16,11 +16,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -85,6 +83,13 @@ public class AuthenticationController {
         response.setHeader(HttpHeaders.SET_COOKIE, expiredCookie.toString());
 
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDTO> getMyData(@AuthenticationPrincipal User authenticatedUser) {
+        UserResponseDTO response = new UserResponseDTO(authenticatedUser);
+
+        return ResponseEntity.ok().body(response);
     }
 
     private void setAuthCookie(HttpServletResponse response, User user) {
